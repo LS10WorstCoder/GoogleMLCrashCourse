8b22d195bac30dc62f72702ee6e4514d499943d3
LS10WorstCoder <ofc.tedsold@gmail.com>
2024-06-10 01:49:35 -0400

    finished pandas tutorial

--- PandasTut.py
+++ PandasTut.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 
-twoD = ([np.random.randint(101,size=4),np.random.randint(101,size=4),np.random.randint(101,size=4)])
+twoD = (np.random.randint(low = 0, high=101,size=(3,4)))
 colNames = ['Eleanor','Chidi','Tahani','Jason']
 myDataFrame = pd.DataFrame(data=twoD,columns=colNames)
 print(myDataFrame)
-print(myDataFrame.iloc[[1]])
+print(myDataFrame['Eleanor'][1])
+myDataFrame["Janet"] = myDataFrame["Tahani"] + myDataFrame["Jason"]
+print(myDataFrame)
