a353513236a0f4ea2d7719102357e6e867ba5ac1
LS10WorstCoder <ofc.tedsold@gmail.com>
2024-06-10 00:43:02 -0400

    noise added

--- LinearDatasetPractice.py
+++ LinearDatasetPractice.py
@@ -2,5 +2,7 @@ import numpy as np
 
 featureArray = np.arange(6, 21)
 labelArray = 3*(featureArray)+4
+noiseArray = labelArray + (np.random.random([15]) * 4) - 2
 print(featureArray)
 print(labelArray)
+print(noiseArray)
