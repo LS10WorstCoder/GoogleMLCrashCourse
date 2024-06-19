b40e7773bc42ba00b8096c12ab001c601af6e3db
LS10WorstCoder <ofc.tedsold@gmail.com>
2024-06-18 23:58:18 -0400

    task 1 and 2

---
+++ BinaryClassification/task1.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras
+from keras import layers
+from matplotlib import pyplot as plt
+
+# The following lines adjust the granularity of reporting.
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+# tf.keras.backend.set_floatx('float32')
+
+train_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
+test_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_test.csv")
+train_df = train_df.reindex(np.random.permutation(train_df.index)) # shuffle the training set
+
+# Calculate the Z-scores of each column in the training set and
+# write those Z-scores into a new pandas DataFrame named train_df_norm.
+train_df_mean = train_df.mean()
+train_df_std = train_df.std()
+train_df_norm = (train_df - train_df_mean)/train_df_std
+
+test_df_norm = (test_df - train_df_mean) / train_df_std
+
+threshold = 265000 # This is the 75th percentile for median house values.
+train_df_norm["median_house_value_is_high"] = (train_df["median_house_value"] > threshold).astype(float) #my code
+test_df_norm["median_house_value_is_high"] = (test_df["median_house_value"] > threshold).astype(float) #my code
+
+inputs = {
+# Features used to train the model on.
+  'median_income': tf.keras.Input(shape=(1,)),
+  'total_rooms': tf.keras.Input(shape=(1,))
+}
+
+#@title Define the functions that create and train a model.
+def create_model(my_inputs, my_learning_rate, METRICS):
+  # Use a Concatenate layer to concatenate the input layers into a single tensor.
+  # as input for the Dense layer. Ex: [input_1[0][0], input_2[0][0]]
+  concatenated_inputs = tf.keras.layers.Concatenate()(my_inputs.values())
+  dense = layers.Dense(units=1, name='dense_layer', activation=tf.sigmoid)
+  dense_output = dense(concatenated_inputs)
+  """Create and compile a simple classification model."""
+  my_outputs = {
+    'dense': dense_output,
+  }
+  model = tf.keras.Model(inputs=my_inputs, outputs=my_outputs)
+
+  # Call the compile method to construct the layers into a model that
+  # TensorFlow can execute.  Notice that we're using a different loss
+  # function for classification than for regression.
+  model.compile(optimizer=tf.keras.optimizers.experimental.RMSprop(learning_rate=my_learning_rate),
+                loss=tf.keras.losses.BinaryCrossentropy(),
+                metrics=METRICS)
+  return model
+
+
+def train_model(model, dataset, epochs, label_name,
+                batch_size=None, shuffle=True):
+  """Feed a dataset into the model in order to train it."""
+
+  # The x parameter of tf.keras.Model.fit can be a list of arrays, where
+  # each array contains the data for one feature.  Here, we're passing
+  # every column in the dataset. Note that the feature_layer will filter
+  # away most of those columns, leaving only the desired columns and their
+  # representations as features.
+  features = {name:np.array(value) for name, value in dataset.items()}
+  label = np.array(features.pop(label_name))
+  history = model.fit(x=features, y=label, batch_size=batch_size,
+                      epochs=epochs, shuffle=shuffle)
+
+  # The list of epochs is stored separately from the rest of history.
+  epochs = history.epoch
+
+  # Isolate the classification metric for each epoch.
+  hist = pd.DataFrame(history.history)
+
+  return epochs, hist
+
+#@title Define the plotting function.
+def plot_curve(epochs, hist, list_of_metrics):
+  """Plot a curve of one or more classification metrics vs. epoch."""
+  # list_of_metrics should be one of the names shown in:
+  # https://www.tensorflow.org/tutorials/structured_data/imbalanced_data#define_the_model_and_metrics
+
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Value")
+
+  for m in list_of_metrics:
+    x = hist[m]
+    plt.plot(epochs[1:], x[1:], label=m)
+
+  plt.legend()
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 100
+label_name = "median_house_value_is_high"
+classification_threshold = 0.35
+
+# Establish the metrics the model will measure.
+METRICS = [
+           tf.keras.metrics.BinaryAccuracy(name='accuracy',
+                                           threshold=classification_threshold),
+          ]
+
+# Establish the model's topography.
+my_model = create_model(inputs, learning_rate, METRICS)
+
+# To view a PNG of this model's layers, uncomment the call to
+# `tf.keras.utils.plot_model` below. After running this code cell, click
+# the file folder on the left, then the `my_classification_model.png` file.
+# tf.keras.utils.plot_model(my_model, "my_classification_model.png")
+
+# Train the model on the training set.
+epochs, hist = train_model(my_model, train_df_norm, epochs,
+                           label_name, batch_size)
+
+# Plot a graph of the metric(s) vs. epochs.
+list_of_metrics_to_plot = ['accuracy']
+
+plot_curve(epochs, hist, list_of_metrics_to_plot)
+
+features = {name:np.array(value) for name, value in test_df_norm.items()}
+label = np.array(features.pop(label_name))
+
+my_model.evaluate(x = features, y = label, batch_size=batch_size)
